1907d92087923064f73f21e8780ed76034b6e9bf
felipebns <felipebns@al.insper.edu.br>
2025-03-13 00:35:19 -0300

    melhor

--- exe3/main.c
+++ exe3/main.c
@@ -5,25 +5,11 @@
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile uint32_t press_start_time = 0;
-volatile bool led_state = false;
-volatile bool button_held = false;
+volatile int flag_button = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (gpio == BTN_PIN_R) {
-        if (events & GPIO_IRQ_EDGE_FALL) { // Botão pressionado
-            press_start_time = to_ms_since_boot(get_absolute_time());
-            button_held = true;
-        } else if (events & GPIO_IRQ_EDGE_RISE) { // Botão solto
-            if (button_held) {
-                uint32_t press_duration = to_ms_since_boot(get_absolute_time()) - press_start_time;
-                if (press_duration > 500) {
-                    led_state = !led_state;
-                    gpio_put(LED_PIN_R, led_state);
-                }
-                button_held = false;
-            }
-        }
+    if (gpio == BTN_PIN_R && events & GPIO_IRQ_EDGE_FALL) {
+        flag_button = 1;
     }
 }
 
@@ -39,9 +25,19 @@ int main() {
     gpio_pull_up(BTN_PIN_R);
 
     gpio_set_irq_enabled_with_callback(
-        BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
+        BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
     while (true) {
-        tight_loop_contents(); // Mantém o loop ativo
+        if (flag_button) {
+            uint32_t start_ms = to_ms_since_boot(get_absolute_time());
+            while (!gpio_get(BTN_PIN_R)) {
+                tight_loop_contents();
+            }
+            uint32_t delta = to_ms_since_boot(get_absolute_time()) - start_ms;
+            if (delta > 500) {
+                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+            }
+            flag_button = 0;
+        }
     }
 }
